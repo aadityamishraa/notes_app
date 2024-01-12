78a88818bd73845700ae0f6e1b0a79b4104fd098
aadityamishraa <aadityamishra216@gmail.com>
2024-01-12 12:19:27 +0530

    code refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
+import NewNote from "./NewNote";
 
 function App() {
-
-
   return (
-    <Routes>
-      <Route path='/' element={<h1>hello</h1>} />
-      <Route path='/new' element={<h1>New</h1>} />
-      <Route path='/:id'>
-        <Route index element={<h1>Show</h1>}/>
-        <Route path='edit' element={<h1>Edit</h1>}/>
-      </Route>
-      <Route path='*' element={<Navigate to='/'></Navigate>} />
-    </Routes>
-  )
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>hello</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/"></Navigate>} />
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
