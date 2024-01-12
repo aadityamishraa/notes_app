b8fbefa712295535fe044bbfa8ba391612092b9f
aadityamishraa <aadityamishra216@gmail.com>
2024-01-12 14:39:46 +0530

    note type defined

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,19 +1,26 @@
 import NoteForm from "./NoteForm"
 
+export type Note ={
+   id: string
+} & NoteData
 
-type Note ={
-  data: String
-};
-
-type NewNote={
-  title: string,
+export type NoteData ={
+  title: string
   markdown: string
-};
+  tags: Tags[]
+}
+
+export type Tags ={
+  id: string
+  label: string
+}
+
+
 const NewNote = () => {
   return (
     <>
         <h1 className="mb-4">New Note</h1>
-        <NoteForm></NoteForm>
+        <NoteForm onSubmit={(NoteData)=>NoteData}></NoteForm>
     </>
   )
 }
